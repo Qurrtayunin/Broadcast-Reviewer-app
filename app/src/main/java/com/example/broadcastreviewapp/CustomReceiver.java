be5b5409cd54c773524da1af6735b5cb099ce912
Qurrota Ayunin <qurrotaayunin36@gmail.com>
2020-02-25 08:43:37 +0700

    first commit

---
+++ app/src/main/java/com/example/broadcastreviewapp/CustomReceiver.java
@@ -0,0 +1,28 @@
+package com.example.broadcastreviewapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class CustomReceiver extends BroadcastReceiver {
+
+    static final String ACTION_CUSTOM_BROADCAST="com.example.broadcastreviewerapp.ACTION_CUSTOM_BROADCAST";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String intentAction=intent.getAction();
+        String message=null;
+        switch (intentAction){
+            case Intent.ACTION_POWER_CONNECTED:
+                message="Power Connected!";
+                break;
+            case Intent.ACTION_POWER_DISCONNECTED:
+                message="Power Disconnected!";
+                break;
+            case ACTION_CUSTOM_BROADCAST:
+                message=intent.getStringExtra("DATA");
+        }
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+}
---
+++ app/src/main/java/com/example/broadcastreviewapp/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.broadcastreviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import android.app.DatePickerDialog;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import com.example.broadcastreviewerapp.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    private CustomReceiver mReceiver=new CustomReceiver();
+    private ComponentName mReceiverComponentName;
+    private PackageManager mPackageManager;
+
+    Button broadcastButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        broadcastButton=findViewById(R.id.broadcastButton);
+        mReceiverComponentName=new ComponentName(this,CustomReceiver.class);
+        mPackageManager=getPackageManager();
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(mReceiver,new IntentFilter(CustomReceiver.ACTION_CUSTOM_BROADCAST));
+        broadcastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendCustomBroadcast();
+            }
+        });
+    }
+    private void SendCustomBroadcast(){
+        Intent broadcastIntent=new Intent(CustomReceiver.ACTION_CUSTOM_BROADCAST);
+        broadcastIntent.putExtra("DATA","Data Broadcast");
+        LocalBroadcastManager.getInstance(this).sendBroadcast(broadcastIntent);
+    }
+
+    @Override
+    protected void onStart() {
+        mPackageManager.setComponentEnabledSetting(mReceiverComponentName,PackageManager.COMPONENT_ENABLED_STATE_ENABLED,PackageManager.DONT_KILL_APP);
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mPackageManager.setComponentEnabledSetting(mReceiverComponentName,PackageManager.COMPONENT_ENABLED_STATE_DISABLED,PackageManager.DONT_KILL_APP);
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mReceiver);
+        super.onDestroy();
+    }
+}
